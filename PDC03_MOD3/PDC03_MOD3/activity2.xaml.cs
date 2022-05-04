f21d7f753dfb1f7016581ccd8cad91a882ae20d8
Aaron Chris Deloso <deloso.aaronchris@auf.edu.ph>
2022-05-04 18:19:06 +0800

    Add project files.

---
+++ PDC03_MOD3/PDC03_MOD3/activity2.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PDC03_MOD3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class activity2 : ContentPage
+    {
+        public activity2()
+        {
+            InitializeComponent();
+        }
+    }
+}
